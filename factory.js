7fdcfaf58946ca0e9cf59d0ee0e2b64d98ec6ae9
Cwdrian <xcharlesdrian@gmail.com>
2023-09-10 22:56:29 -0400

    first attempt

--- factory.js
+++ factory.js
@@ -1,3 +1,18 @@
 class Factory{
-    
+    constructor(location, owner){
+
+        this.location = location
+        this.owner = owner
+    }
+    createVehicle(){
+        super(vehicle)
+        return this.vehicle + " " + "vehicle"
+    }
+    listVehicles(){
+        return this.Factory + " " + "drives"
+    }
+    paintVehicle(){
+        return this.vin + " " + "vin"
+        return this.paint + " " + "paint"
+    }
 }
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,17 @@
-class Vehicle {
-    
-}
+class Vehicle{
 
+    constructor(make, model, color, price, vin,){
+        this.make = make
+        this.model = model
+        this.color = color
+        this.price = price
+        this.vin = vin
+    
+    }
+    paint(paintColor) {
+            this.paint = paintColor;
+}
+    drive(){
+        return this.vehicle + " " + "drives"
+    }
+    }
